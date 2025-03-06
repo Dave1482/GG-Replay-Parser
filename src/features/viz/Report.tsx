e26f283fa4bf07a763482d96a32e1402f8493aa4
Dave T. <thedave1482@gmail.com>
2025-03-05 23:41:51 -0600

    Change stats to show wins instead of total goals

--- src/features/viz/Report.tsx
+++ src/features/viz/Report.tsx
@@ -35,8 +35,8 @@ export const Report = () => {
           <h3 className="text-2xl">Score:</h3>
         </div>
         <TeamScores
-          team0score={totalTeamGoals.team0}
-          team1score={totalTeamGoals.team1}
+          team0score={stats.team0Wins}//{totalTeamGoals.team0}
+          team1score={stats.team1Wins}//{totalTeamGoals.team1}
           currentPage={currentPage}
           totalPages={totalPages}
           onPageChange={setCurrentPage}
